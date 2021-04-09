798b5b597eecace88a42055c9cbe8deb8be21b0f
Dorukhan Tokay <tokay.dorukhan@hotmail.com>
2021-04-10 01:54:57 +0300

    forgot to hash twice

--- tiny-lib/SHA256d.cpp
+++ tiny-lib/SHA256d.cpp
@@ -7,14 +7,21 @@
 
 std::vector<uint8_t> SHA256d::HashBinary(const std::vector<uint8_t>& buffer)
 {
-    std::vector<uint8_t> hash(SHA256_DIGEST_LENGTH);
+    std::vector<uint8_t> hash1(SHA256_DIGEST_LENGTH);
 
     SHA256_CTX sha256;
+
     SHA256_Init(&sha256);
     SHA256_Update(&sha256, buffer.data(), buffer.size());
-    SHA256_Final(hash.data(), &sha256);
+    SHA256_Final(hash1.data(), &sha256);
 
-    return hash;
+    std::vector<uint8_t> hash2(SHA256_DIGEST_LENGTH);
+
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, hash1.data(), hash1.size());
+    SHA256_Final(hash2.data(), &sha256);
+
+    return hash2;
 }
 
 std::string SHA256d::BinaryHashToString(const std::vector<uint8_t>& buffer)
